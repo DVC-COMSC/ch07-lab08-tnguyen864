50777a0f68869d4c99e7f40518775578658761b0
Thuy Nguyen <thuicide@gmail.com>
2022-10-10 03:42:54 -0700

    Chap7 Lab8

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-# ******************************
-# Make your Code
-# ******************************
+# List for 5 int values
+numbers = []
+
+# Get values for list
+print("Enter 5 integer values:")
+i = 0
+for i in range(5):
+    num = int(input())
+    numbers.append(num)
+
+# Sort list 
+numbers.sort()
+
+# Print sorted list
+print("List:", numbers)
+
+# Get new value to add to list 
+newNum = int(input("Enter value to add to list: "))
+numbers.append(newNum)
+
+# Sort list again
+numbers.sort()
+
+# Print updated and sorted list
+print("Updated list:", numbers)
